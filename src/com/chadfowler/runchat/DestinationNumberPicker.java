ac5ee6952d826ee3ad9d3f61814c3d985a0f31fc
Chad Fowler <chad@chadfowler.com>
2010-07-11 11:44:31 -0800

    working on prefs menus

---
+++ src/com/chadfowler/runchat/DestinationNumberPicker.java
@@ -0,0 +1,12 @@
+package com.chadfowler.runchat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DestinationNumberPicker extends Activity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+	  super.onCreate(savedInstanceState);
+	  setContentView(R.layout.destination_number);
+  }
+}
